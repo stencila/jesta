6d4aca617ef2896e5e4738c09ed004b09f0cdc2e
Nokome Bentley <nokome@stenci.la>
2021-01-21 11:59:55 +1300

    fix(*): Export symbols
    For use by other developers who are extending Jesta

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+import { cli } from './cli'
 import { dispatch } from './dispatch'
 import { manifest } from './manifest'
-import { cli } from './cli'
+
+export * from './cli'
+export * from './dispatch'
+export * from './manifest'
+export * from './methods'
+export * from './register'
+export * from './serve'
 
 if (require.main === module) cli(__filename, manifest, dispatch)
