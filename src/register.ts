711154afa91b9cefdc1b13745614da6672540b8b
Nokome Bentley <nokome@stenci.la>
2021-01-20 23:32:55 +1300

    feat(Register): Implement registration of plugin

--- src/register.ts
+++ src/register.ts
@@ -1,8 +1,35 @@
-import { ManifestFunction } from './manifest'
+import fs from 'fs'
+import { homedir } from 'os'
+import path from 'path'
+import { promisify } from 'util'
+import { Manifest } from './manifest'
+
+const writeFileAsync = promisify(fs.writeFile)
+const mkdirAsync = promisify(fs.mkdir)
 
 /**
  * Register the plugin.
  */
-export const register = async (manifester: ManifestFunction): Promise<void> => {
-  // TODO Write the manifest to correct location
+export const register = async (manifest: Manifest): Promise<void> => {
+  const { name } = manifest
+  const { env, platform } = process
+
+  // Based on https://github.com/sindresorhus/env-paths/blob/master/index.js
+  const dir = path.join(
+    ...(platform === 'win32'
+      ? [
+          env.APPDATA || path.join(homedir(), 'AppData', 'Roaming'),
+          'Stencila',
+          'Config',
+          'Plugins',
+        ]
+      : platform == 'darwin'
+      ? [homedir(), 'Library', 'Preferences', 'Stencila', 'Plugins']
+      : [homedir(), '.config', 'stencila', 'plugins'])
+  )
+  if (!fs.existsSync(dir)) await mkdirAsync(dir, { recursive: true })
+
+  const file = path.join(dir, `${name}.json`)
+
+  await writeFileAsync(file, JSON.stringify(manifest, null, '  '), 'utf8')
 }
