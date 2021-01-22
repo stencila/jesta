79d7911bb6c25408193f09030bf8fcd8e28521f3
Nokome Bentley <nokome@stenci.la>
2021-01-22 15:12:13 +1300

    fix(Vars): Exclude globals

--- src/methods/vars.ts
+++ src/methods/vars.ts
@@ -1,9 +1,20 @@
 import { nodeType } from '@stencila/schema'
 import { context } from '../utilities/session'
 
+// A list of global variable names e.g. setTimeout which should not
+// be included in the list of variables
+const globals = Object.keys(globalThis)
+
+/**
+ * List variables of the current stencil.
+ *
+ * Returns a map of the name and type of
+ * the current stencil's variables.
+ */
 export const vars = (): Record<string, string> => {
   return Object.entries(context).reduce(
-    (prev, [name, value]) => ({ ...prev, [name]: nodeType(value) }),
+    (prev, [name, value]) =>
+      globals.includes(name) ? prev : { ...prev, [name]: nodeType(value) },
     {}
   )
 }
